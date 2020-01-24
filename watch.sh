d4956fb37343b84af4db74727712325f8f431d35
marcelo lopes de sousa lima <marcelo.lopesousa@gmail.com>
2020-01-24 09:55:50 -0400

    Update watch.sh

--- watch.sh
+++ watch.sh
@@ -1,6 +1,6 @@
 for p in $(ps -eo comm,pid |egrep 'moni.*sh' |awk '{print $2}'); do
-        TT=$(ps -p $p -o etime |tail -1 |sed 's/ //g');
-        M=$(echo $TT |awk -F':' '{print $1}');
+        TT=$(ps -p $p -o etime |tail -1 |sed 's/ //g')
+        M=$(echo $TT |awk -F':' '{print $1}')
         if [ $TT ]; then
 
                 # Se o elapsed time tiver mais que 5 caracters, mais de uma hora
